6ca24e408f1c16411abf140f9928cfe4ddec3f7d
Elena Doumon <eldoua@gigl.polymtl.ca>
2023-11-07 17:47:30 -0500

    TESTING

--- myscript.py
+++ myscript.py
@@ -1,4 +1,5 @@
 import os
+os.system('git log')
 os.system('git bisect start')
 os.system('git bisect good e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect bad c1a4be04b972b6c17db242fc37752ad517c29402')
