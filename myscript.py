3237b5464dfddcd5dcfdc5989ca26b3877c1363a
Elena Doumon <eldoua@gigl.polymtl.ca>
2023-11-07 17:25:59 -0500

    question C fixed

--- myscript.py
+++ myscript.py
@@ -1,4 +1,4 @@
 import os
 
-os.system(f'git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 c1a4be04b972b6c17db242fc37752ad517c29402')
+os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 c1a4be04b972b6c17db242fc37752ad517c29402')
 os.system('git bisect run python manage.py test')
